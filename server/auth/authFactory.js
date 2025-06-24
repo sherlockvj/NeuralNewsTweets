08c38a66e801d4f4cace67121ed589d8aedb210f
sherlockvj <mailmesomyajit@gmail.com>
2025-06-24 23:13:07 +0530

    Code cleanup

--- server/auth/authFactory.js
+++ server/auth/authFactory.js
@@ -1,15 +1,9 @@
 import EmailAuthStrategy from "./strategies/email.strategy.js";
-// import GoogleAuthStrategy from "./strategies/google.strategy.js";
-// import TwitterAuthStrategy from "./strategies/twitter.strategy.js";
 
 export const AuthFactory = (type) => {
   switch (type) {
     case "email":
       return new EmailAuthStrategy();
-    // case "google":
-    //   return new GoogleAuthStrategy();
-    // case "twitter":
-    //   return new TwitterAuthStrategy();
     default:
       throw new Error("Unsupported auth strategy");
   }
